6cadfb2acb40623a27fcfc6e998c913a05bf3dae
Mohammad Aamir <121339457+aamir443@users.noreply.github.com>
2025-06-15 07:16:41 +0000

    rest operator

---
+++ basicJs/functions.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(a, b){
+    if(typeof a !== 'number' || typeof b !== 'number') {
+        return; // Return undefined if inputs are not numbers
+    }
+    return a+b;
+}
+console.log(addTwoNumbers(5, 'a'));
+
+// rest operator
+function calculateCartPrice(val1, val2, ...price){  
+    return price;
+} 
+// rest operator allows us to pass any number of arguments
+// and it will be stored in an array called price
+console.log(calculateCartPrice(100, 200, 300, 400, 500));
+
+// 100 will be val1 and 200 will be val2 rest will be strored in price array
