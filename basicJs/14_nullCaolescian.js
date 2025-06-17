aae2896d9da3350c5ca0de2c7166a57a1dd00288
Mohammad Aamir <121339457+aamir443@users.noreply.github.com>
2025-06-17 16:49:45 +0000

    ?? operator

---
+++ basicJs/14_nullCaolescian.js
@@ -0,0 +1,10 @@
+// ?? null colescing operator
+// used of ?? // to provide a default value when the left-hand side is null or undefined
+    let value = null ?? 30;
+    console.log(value);
+
+        let value2 = undefined ?? 30;
+    console.log(value2);
+
+          let value3 = 10 ?? 30 ??40;
+    console.log(value3);
